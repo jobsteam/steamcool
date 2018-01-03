f3285ff31bcc86c87411717f35a7db16168562a2
jobsteam <jobsteam@yandex.ru>
2018-01-03 14:07:45 +0000

    Рабочий deploy

--- fabfile.py
+++ fabfile.py
@@ -8,9 +8,10 @@ class Fabric(DjangoFabric):
     host = '89.223.26.151'
     app_name = 'key_market'
     repository = 'git@github.com:jobsteam/steamcool.git'
-    remote_db_name = 'stationery'
+    remote_db_name = 'steamcool'
     local_db_name = 'key_market'
     use_bower = True
+    user = 'dimi'
 
 
 __all__ = add_class_methods_as_functions(Fabric(), __name__)
